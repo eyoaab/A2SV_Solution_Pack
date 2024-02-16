f392afbe419e5fd09b7988e3dbc4a232ca49fcca
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-02-16 18:11:09 +0300

    Add solution for Move Zeroes

---
+++ progress_sheet/leetcode/move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left=0
+        for right in range(len(nums)):
+            if(nums[left]==0 and nums[right]!=0):
+                nums[left],nums[right]=nums[right],nums[left]
+            if(nums[left]!=0):
+                left+=1     
