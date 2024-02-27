43abd8fb8e01ef48c96cda92ff1469fb5b48c0e6
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-02-27 10:51:14 +0300

    Add solution for Reverse String

--- progress_sheet/leetcode/reverse-string.py
+++ progress_sheet/leetcode/reverse-string.py
@@ -1,5 +1,19 @@
 class Solution:
+    def help(self,s,l,r):
+        if l>=r:
+            return
+        s[l],s[r]=s[r],s[l]
+        self.help(s,l+1,r-1)
     def reverseString(self, s: List[str]) -> None:
-        s=(s).reverse()
-       
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l=-1
+        r=len(s)
+        self.help(s,l+1,r-1)    
+        
+         
+
+      
+        
         
