6f7967dfc7df022dc4bef6772e3897decffab5d1
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-03-01 14:42:54 +0300

    Add solution for Partition Labels

---
+++ progress_sheet/leetcode/partition-labels.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        l,r,t=0,0,0
+        ans=[]
+        last={}
+        for i in range(len(s)):
+            last[s[i]]=i
+        while l<len(s):
+            r=max(r,last[s[l]])
+            if l==r:
+                ans.append(r-t+1)
+                t=l+1
+            l+=1
+        return ans          
+
+           
+      
+     
+            
+        
