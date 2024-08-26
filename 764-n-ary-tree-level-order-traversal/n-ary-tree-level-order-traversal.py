e7aeee3dc45ed1bdea990bcadf7158995004f453
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-08-26 21:44:39 +0300

    Time: 48 ms (46.07%) | Memory: 18.1 MB (96.30%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.py
@@ -0,0 +1,27 @@
+#Python3 Code
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                for child in node.children:
+                    queue.append(child)
+            result.append(level)
+        
+        return result
