0c7b14760e0d632c06d3b041a89d1dc6dc15e5c6
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-09-02 16:54:38 +0300

    Time: 967 ms (79.47%) | Memory: 32.9 MB (25.17%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
@@ -1,13 +1,11 @@
-def llcm(x: int, y: int) -> int:
-    return x * y // gcd(x, y)
-
 class Solution:
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
         stack = []
 
         for num in nums:
-            while stack and gcd(stack[-1], num) > 1:
-                num = llcm(stack.pop(), num)
+            while stack and gcd(stack[-1],num)  > 1:
+                num = lcm(stack.pop(),num)
+
             stack.append(num)
 
-        return stack
+        return stack          
