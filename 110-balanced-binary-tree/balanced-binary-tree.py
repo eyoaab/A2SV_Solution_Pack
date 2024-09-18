a2fee0056b7537ae87c813357b17a7214b90b9e9
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-09-18 11:20:53 +0300

    Time: 41 ms (79.28%) | Memory: 17.6 MB (65.22%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isBalanced(self, root):
+        return (self.Height(root) >= 0)
+    
+    def Height(self, root):
+        if root is None: 
+            return 0
+            
+        leftheight = self.Height(root.left)
+        rightheight = self.Height(root.right)
+
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:
+            return -1
+        return max(leftheight, rightheight) + 1
