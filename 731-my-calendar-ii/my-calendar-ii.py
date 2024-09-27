e9b7d9e38f8cddaa117e22958ffebe560e441b21
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-09-27 12:57:07 +0300

    Time: 306 ms (70.91%) | Memory: 17.1 MB (97.40%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.py
@@ -0,0 +1,26 @@
+class MyCalendarTwo:
+    def __init__(self):
+        self.bookings = []
+        
+    def book(self, start: int, end: int) -> bool:
+        for a, b in self.bookings:
+            if start < b and end > a:
+                new_start = max(a, start)
+                new_end = min(b, end)
+                
+                if self.check(new_start, new_end):
+                    return False  
+        
+        self.bookings.append((start, end))
+        return True  
+    
+    def check(self, start: int, end: int) -> bool:
+        overlap_count = 0
+        
+        for a, b in self.bookings:
+            if start < b and end > a:
+                overlap_count += 1
+                if overlap_count == 2:
+                    return True 
+        
+        return False  
