a89b182a105e6670c08a4cd89505df664ad2d948
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-10-25 18:11:43 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (38.88%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.py
@@ -0,0 +1,14 @@
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        if not set(b).issubset(set(a)):
+            return -1
+        
+        repeat_count = -(-len(b) // len(a))  
+     
+        repeated_a = a * repeat_count
+        if b in repeated_a:
+            return repeat_count
+        elif b in repeated_a + a:
+            return repeat_count + 1
+        
+        return -1
