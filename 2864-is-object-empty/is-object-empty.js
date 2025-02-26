4301b6f4ace8fc843a23d10d066d01b82619f89b
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2025-02-26 13:10:39 +0300

    Time: 49 ms (68.10%) | Memory: 53.9 MB (5.37%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -1,11 +1,4 @@
-/**
- * @param {Object|Array} obj
- * @return {boolean}
- */
-function isEmpty(obj) {
-  if (Array.isArray(obj)) {
-    return obj.length === 0;
-  } else {
-    return JSON.stringify(obj) === "{}";
-  }
-}
+
+var isEmpty = function(obj) {
+ return Object.keys(obj).length === 0
+};
