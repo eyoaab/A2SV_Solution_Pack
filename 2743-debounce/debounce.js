b2e52f6a756db2b4e57948bffed8b55d5a9177ff
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2025-02-26 13:08:08 +0300

    Time: 52 ms (83.97%) | Memory: 54.2 MB (5.94%) - LeetSync

---
+++ 2743-debounce/debounce.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timeout;
+
+    return function(...args) {
+        clearTimeout(timeout);
+        
+        timeout = setTimeout(() => {
+            fn(...args); 
+        }, t);
+    };
+};
+
