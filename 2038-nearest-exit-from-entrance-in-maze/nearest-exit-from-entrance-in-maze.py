669e76b409c6e603c8f6bb1f8d7bbaa273fcbf93
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2025-02-28 21:03:28 +0300

    Time: 127 ms (19.86%) | Memory: 20.2 MB (17.31%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -3,30 +3,32 @@ class Solution:
         if maze[entrance[0]][entrance[1]] == "+":
             return -1
 
-        roww, coll = len(maze), len(maze[0])
+        ans = float('inf')
+        roww,coll = len(maze),len(maze[0]) 
+        def isBound(r,c): 
+            return (0 <= r < roww) and (0 <= c < coll)  
 
-        def isBound(r, c):
-            return 0 <= r < roww and 0 <= c < coll
-
-        def isBorder(r, c):
-            return r == 0 or r == roww - 1 or c == 0 or c == coll - 1
-
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        queue = deque()
-        queue.append((entrance[0], entrance[1], 0))
+        def isBorder(r,c): 
+            # print(r,c)/
+            return (r == 0 or r == roww - 1 ) or (c == 0 or c == coll - 1 )
+        
         visited = set()
-        visited.add((entrance[0], entrance[1]))  
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        queue = deque()
+        queue.append([entrance[0],entrance[1],0])
+
 
         while queue:
-            row, col, count = queue.popleft()
 
+            row,col,count = queue.popleft()
+            # print(row,col,count)
             if isBorder(row, col) and [row, col] != entrance:
                 return count
 
-            for dx, dy in directions:
-                new_row, new_col = row + dx, col + dy
-                if isBound(new_row, new_col) and maze[new_row][new_col] == "." and (new_row, new_col) not in visited:
-                    queue.append((new_row, new_col, count + 1))
-                    visited.add((new_row, new_col)) 
-
-        return -1
+            visited.add((row,col)) 
+            for dx,dy in directions:
+                if isBound(dx + row,dy + col) and  maze[dx + row][dy + col] == "." and (dx + row,dy + col) not in visited :
+                    queue.append([dx + row,dy + col,count + 1])
+                    visited.add((dx + row,dy + col) )
+                    
+        return -1 
