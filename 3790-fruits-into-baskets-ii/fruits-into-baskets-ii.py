26af5ab1164f9e4fc6448a29fee54682ad3b069a
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2025-08-05 09:37:57 +0300

    Time: 27 ms (32.16%) | Memory: 18 MB (17.40%) - LeetSync

---
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: list[int], baskets: list[int]) -> int:
+        n = len(fruits)
+        alloted = 0
+
+        for i in range(n):
+            for j in range(n):
+                if fruits[i] <= baskets[j]:
+                    baskets[j] = -1  
+                    alloted += 1
+                    break
+                    
+        return n - alloted
