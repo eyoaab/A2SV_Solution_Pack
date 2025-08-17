2bf9077bdc66eaed260b908b0fcc695a42f1c5fa
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2025-08-17 10:56:19 +0300

    Time: 23 ms (72.90%) | Memory: 18.1 MB (82.26%) - LeetSync

---
+++ 867-new-21-game/new-21-game.py
@@ -0,0 +1,25 @@
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        if k == 0 or n >= k - 1 + maxPts:
+            return 1.0
+
+        dp = [0.0] * maxPts
+        dp[0] = 1.0
+
+        sum_ = 1.0
+        result = 0.0
+
+        for i in range(1, n + 1):
+            prob = sum_ / maxPts
+
+            if i < k:
+                sum_ += prob
+            else:
+                result += prob
+
+            if i >= maxPts:
+                sum_ -= dp[i % maxPts]
+
+            dp[i % maxPts] = prob
+
+        return result
