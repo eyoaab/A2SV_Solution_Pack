fb15c387c81d9b2c947dc393fe90f0c7a44f7221
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2026-04-05 19:41:49 +0300

    Time: 23 ms (11.29%) | Memory: 19.2 MB (67.40%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x,y=0,0
+        for i in moves:
+            if i== 'U':
+                y+=1
+            if i== 'D':
+                y-=1
+            if i== 'R':
+                x+=1
+            if i== 'L':
+                x-=1
+        return x==0 and y==0
+                              
+
+        
