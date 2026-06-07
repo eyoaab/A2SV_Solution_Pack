f9f84027cddb407e9dd75f4daebbed963deb00a2
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2026-06-07 09:16:56 +0300

    Time: 199 ms (16.46%) | Memory: 43.6 MB (6.50%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
@@ -1,27 +1,30 @@
 class Solution:
     def createBinaryTree(self, descriptions: list[list[int]]) -> Optional[TreeNode]:
-        def construct_tree(cur_node_val):
-            new_node = TreeNode(cur_node_val)
-            if cur_node_val in children_hashmap:
-                if children_hashmap[cur_node_val][0] is not None:
-                    new_node.left = construct_tree(children_hashmap[cur_node_val][0])
-                if children_hashmap[cur_node_val][1] is not None:
-                    new_node.right = construct_tree(children_hashmap[cur_node_val][1])
-            return new_node
+        def build_tree(curr):
+            new_ = TreeNode(curr)
+            if curr in store:
+                if store[curr][0] is not None:
+                    new_.left = build_tree(store[curr][0])
 
-        children_set = set()
-        children_hashmap: dict[int, list[int]] = {}
+                if store[curr][1] is not None:
+                    new_.right = build_tree(store[curr][1])
+                    
+            return new_
+
+        childrens = set()
+        store: dict[int, list[int]] = {}
 
         for parent, child, isleft in descriptions:
-            if not parent in children_hashmap:
-                children_hashmap[parent] = [None, None]  # left and right
-            children_set.add(child)
+            if not parent in store:
+                store[parent] = [None, None] 
+            childrens.add(child)
             target = 0 if isleft else 1
-            children_hashmap[parent][target] = child
+            store[parent][target] = child
 
-        for parent in children_hashmap:
-            if parent not in children_set:
-                head_node_val: int = parent
+        for parent in store:
+            if parent not in childrens:
+                head_val = parent
                 break
-        head = construct_tree(head_node_val)
+        head = build_tree(head_val)
+
         return head
